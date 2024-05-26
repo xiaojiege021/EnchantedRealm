14ae70b6f23cd3b7330f83e6dd1f9003d3fd4152
xiaojiege021 <nrj021@163.com>
2024-05-26 17:35:30 +0800

    1

--- EnchantedRealmServer/obj/Debug/net8.0/EnchantedRealmServer.AssemblyInfo.cs
+++ EnchantedRealmServer/obj/Debug/net8.0/EnchantedRealmServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnchantedRealmServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26830532fbe70e3c73621bd4611bd997522ea87d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnchantedRealmServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnchantedRealmServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
